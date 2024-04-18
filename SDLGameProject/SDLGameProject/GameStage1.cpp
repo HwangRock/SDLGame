5df8133a46b236b3430eab5721c6d550e69d538a
can378 <yunji378mm@naver.com>
2024-04-18 23:49:36 +0900

    Add Intro,Ending
    인트로, 엔딩 phase추가

---
+++ SDLGameProject/SDLGameProject/GameStage1.cpp
@@ -0,0 +1,167 @@
+#include "Game.h"
+#include "GameStage1.h"
+
+
+//add
+#include <iostream>
+#include <vector>
+#include <windows.h>
+
+#include "DogCatGame.h" 
+#include "SDL_image.h"
+#include "G2W.h"
+#include "math.h"
+
+
+
+extern int g_current_game_phase;
+extern bool g_flag_running;
+extern SDL_Renderer* g_renderer;
+extern SDL_Window* g_window;
+extern float g_timestep_s;
+
+Terrain wall3(400, 200, 20, 500);
+Terrain wall4(100, 300, 600, 50);
+Terrain default1(0, 680, 1000, 20);
+Terrain default2(0, 0, 1000, 20);
+Terrain default3(0, 0, 20, 700);
+Terrain default4(980, 0, 20, 700);
+std::vector<Terrain>walls = { wall3,wall4,default1,default2,default3,default4 };
+
+
+
+//InitGame
+Stage1::Stage1()
+{
+	g_flag_running = true;
+
+
+	//Window
+	SDL_GetWindowSize(g_window, &win_w, &win_h);
+
+
+
+	//Drawing Texture//////////////////////////////
+	//Dog
+	SDL_Surface* g_surface_dog = IMG_Load("../Resources/dog_.png");
+	dogTexture = SDL_CreateTextureFromSurface(g_renderer, g_surface_dog);
+	SDL_FreeSurface(g_surface_dog);
+	dogRect = { 0,0,141,141 };
+
+	//Cat
+	SDL_Surface* g_surface_cat = IMG_Load("../Resources/cat_.png");
+	catTexture = SDL_CreateTextureFromSurface(g_renderer, g_surface_cat);
+	SDL_FreeSurface(g_surface_cat);
+	catRect = { 0,0,90,90 };
+
+	//Wall
+	SDL_Surface* surface_wall = IMG_Load("../Resources/sky.jpg");
+	wallTexture = SDL_CreateTextureFromSurface(g_renderer, surface_wall);
+	SDL_FreeSurface(surface_wall);
+	wallRect = { 0,0,680,808 };
+
+
+
+
+	mouse_win_x_ = 0;
+	mouse_win_y_ = 0;
+}
+
+Stage1::~Stage1()
+{
+	SDL_DestroyTexture(dogTexture);
+	SDL_DestroyTexture(catTexture);
+}
+
+void Stage1::Update()
+{
+	dog->Update(g_timestep_s, walls);
+	cat->Update(g_timestep_s, walls);
+
+}
+
+
+void Stage1::Render()
+{
+	//Background
+	SDL_SetRenderDrawColor(g_renderer, 255, 255, 255, 255);
+	SDL_RenderClear(g_renderer); // clear the renderer to the draw color
+
+
+	//Dog and Cat
+	SDL_RenderCopy(g_renderer, catTexture, &catRect, &cat->pos);
+	SDL_RenderCopy(g_renderer, dogTexture, &dogRect, &dog->pos);
+
+
+	//Wall
+	for (Terrain wall : walls)
+	{
+		SDL_RenderCopy(g_renderer, wallTexture, &wallRect, &wall.pos);
+	}
+
+
+
+
+	SDL_RenderPresent(g_renderer); // draw to the screen
+}
+
+
+
+void Stage1::HandleEvents()
+{
+	SDL_Event event;
+	if (SDL_PollEvent(&event))
+	{
+		dog->HandleEvent(event);
+		cat->HandleEvent(event);
+
+
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			g_flag_running = false;
+			break;
+
+
+		case SDL_KEYDOWN:
+			break;
+
+
+		case SDL_KEYUP:
+			break;
+
+
+		case SDL_MOUSEBUTTONDOWN:
+
+			// If the mouse left button is pressed. 
+			if (event.button.button == SDL_BUTTON_LEFT)
+			{
+				// Get the cursor's x position.
+				mouse_win_x_ = event.button.x;
+				mouse_win_y_ = event.button.y;
+
+
+				double mouse_game_x = W2G_X(mouse_win_x_);
+				double mouse_game_y = W2G_Y(mouse_win_y_);
+
+			}
+			// If the mouse left button is pressed. 
+			if (event.button.button == SDL_BUTTON_LEFT)
+			{
+				g_current_game_phase = PHASE_ENDING;
+			}
+
+
+		case SDL_MOUSEMOTION:
+		{
+			// Get the cursor's x position.
+			mouse_win_x_ = event.button.x;
+			mouse_win_y_ = event.button.y;
+		}
+		break;
+
+		default:
+			break;
+		}
+	}
+}
