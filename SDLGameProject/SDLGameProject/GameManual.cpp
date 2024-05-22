afe80dfd7213d2f0095813c867f3a92416bf0a02
HwangRock <157935545+HwangRock@users.noreply.github.com>
2024-05-22 22:22:33 +0900

    Update GameManual.cpp

--- SDLGameProject/SDLGameProject/GameManual.cpp
+++ SDLGameProject/SDLGameProject/GameManual.cpp
@@ -1,23 +1,21 @@
-#pragma once
-#include "SDL.h"
+#include "Game.h"
 #include "GameManual.h"
+
 
 Manual::Manual()
 {
-	SDL_Surface* temp_surface = IMG_Load("../Resources/control_window.png");
+	// For Texture
+
+	SDL_Surface* temp_surface = IMG_Load("../Resources/manual.png");
 	texture_ = SDL_CreateTextureFromSurface(g_renderer, temp_surface);
 	SDL_FreeSurface(temp_surface);
 
 	SDL_QueryTexture(texture_, NULL, NULL, &source_rectangle_.w, &source_rectangle_.h);
 	destination_rectangle_.x = source_rectangle_.x = 0;
 	destination_rectangle_.y = source_rectangle_.y = 0;
-	destination_rectangle_.w = source_rectangle_.w;
-	destination_rectangle_.h = source_rectangle_.h;
+	destination_rectangle_.w = 1280;
+	destination_rectangle_.h = 700;
 
-	button_rect_.x = 1010;
-	button_rect_.y = 600;
-	button_rect_.w = 215;
-	button_rect_.h = 66;
 }
 
 Manual::~Manual()
@@ -29,6 +27,7 @@ void Manual::Update()
 {
 }
 
+
 void Manual::Render()
 {
 	SDL_SetRenderDrawColor(g_renderer, 255, 255, 255, 255);
@@ -38,6 +37,8 @@ void Manual::Render()
 
 	SDL_RenderPresent(g_renderer); // draw to the screen
 }
+
+
 
 void Manual::HandleEvents()
 {
@@ -50,29 +51,21 @@ void Manual::HandleEvents()
 			g_flag_running = false;
 			break;
 
-		case SDL_MOUSEBUTTONDOWN:
-
-			// If the mouse left button is pressed. 
-			if (event.button.button == SDL_BUTTON_LEFT)
+		case SDL_KEYDOWN:
+			if (event.key.keysym.sym != SDLK_ESCAPE)
 			{
-				int mouse_x = event.button.x;
-				int mouse_y = event.button.y;
-
-				//return button
-				if (mouse_x > button_rect_.x &&
-					mouse_y > button_rect_.y &&
-					mouse_x < button_rect_.x + button_rect_.w &&
-					mouse_y < button_rect_.y + button_rect_.h)
-				{
-					g_current_game_phase = PHASE_ESC;
-				}
+				g_current_game_phase = PHASE_STAGE1;
+				
 			}
-
 			break;
+
+
+		case SDL_KEYUP:
+			break;
+
 
 		default:
 			break;
 		}
 	}
 }
-
