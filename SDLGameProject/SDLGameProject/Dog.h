96c30c255415329272b1563141ba650f71e0ceeb
LEEYEYEONG <93482841+puhoggo@users.noreply.github.com>
2024-05-24 12:59:22 +0900

    Dog.h

--- SDLGameProject/SDLGameProject/Dog.h
+++ SDLGameProject/SDLGameProject/Dog.h
@@ -4,6 +4,8 @@
 #include "Button.h"
 #include "LiquidWall.h"
 #include "ClimbWall.cpp"
+#include "Cushion.h"
+#include "FadeFloor.h"
 
 #include "Choco.cpp"
 #include "Water.cpp"
@@ -32,7 +34,7 @@ public:
 protected:
 	// dog skill variables
 	bool beBlurry_;
-	bool isSkill_;//skill사용가능 여부, dog만 사용
-	int blindOpacity_;//0~255 투명도 정도, -1이면 냄새 안맡고 있는중
+	bool isSkill_;//
+	int blindOpacity_;//
 
 };
